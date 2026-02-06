ae732d265d41ec2664558a56b6e9f099a6c2e08e
Alberto <alberto@alberto-HP-Laptop-15-dw1xxx>
2026-02-06 15:18:04 +0100

    Cria os endpoints do aplicativo accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def register(request):
+    return HttpResponse("Register Page")
+
+def login_view(request):
+    return HttpResponse("Login Page")
+
+def logout_view(request):
+    return HttpResponse("Logout Page")
